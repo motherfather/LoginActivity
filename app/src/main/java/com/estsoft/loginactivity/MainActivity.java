399af67eb8b2d38b7ba63ea89642d74010123c35
yattadog <kickscar@gmail.com>
2016-11-29 03:49:12 +0900

    코드 수정

--- app/src/main/java/com/estsoft/loginactivity/MainActivity.java
+++ app/src/main/java/com/estsoft/loginactivity/MainActivity.java
@@ -2,6 +2,9 @@ package com.estsoft.loginactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        TextView textView = (TextView)findViewById( R.id.tv_signup );
+//        textView.setMovementMethod( LinkMovementMethod.getInstance() );
+//        textView.setText( Html.fromHtml( "<a href=http://www.hellomyoffice.com>Hello My Office 가입하기<;/a>" ));
     }
 }
