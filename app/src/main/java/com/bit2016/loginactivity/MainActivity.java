8010e94afa29234cf2e14df0c3242f55caa72888
motherfather <father@mail.com>
2016-11-29 13:11:49 +0900

    완료

--- app/src/main/java/com/bit2016/loginactivity/MainActivity.java
+++ app/src/main/java/com/bit2016/loginactivity/MainActivity.java
@@ -2,6 +2,9 @@ package com.bit2016.loginactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        TextView textView = (TextView)findViewById( R.id.tv_signup );
-//        textView.setMovementMethod( LinkMovementMethod.getInstance() );
-//        textView.setText( Html.fromHtml( "<a href=http://www.hellomyoffice.com>Hello My Office 가입하기<;/a>" ));
+        // HTML string UI에 적용하기
+        TextView textView = (TextView)findViewById( R.id.tv_signup );
+        textView.setMovementMethod( LinkMovementMethod.getInstance() );
+        textView.setText( Html.fromHtml( getString(R.string.login_url) )); // 모든 태그가 다 되는건 아니다!!
     }
 }
